cb4ed7b9eea1a4e5c0fcf2f611c0bc14936dd2c7
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2025-02-08 15:06:52 +0500

    Environment Variables found

--- tina/config.js
+++ tina/config.js
@@ -11,9 +11,12 @@ export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  // clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // // Get this from tina.io
+  // token: process.env.TINA_TOKEN,
+
+  clientId: 'e38f9851-5845-45fb-8a05-b19dbccde711',
+  token: 'd62b3dc982bff90b6c82111659e91eba927bcbdc',
 
   build: {
     outputFolder: "admin",
